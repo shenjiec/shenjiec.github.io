a87164fb10ed3ec4898d6cdb9e4754e48f0f2772
shenjiec <93009416+shenjiec@users.noreply.github.com>
2022-08-23 16:18:56 +0800

    Update header.js

--- script/header.js
+++ script/header.js
@@ -1,15 +1,16 @@
-header ='<div class="header">\
+header ='<div id="bd" class="bd"></div>\
+<div class="header">\
   <div class="logo">\
 	<a href="index.html">shenjiec</a>\
   </div>\
   <ul id="menu" class="menu">\
 	<li><a href="index.html">Home</a></li>\
-	<li><a href="menu1.html">Build</a></li>\
+	<li><a href="buildmenu.html">Build</a></li>\
 	<li><a href="#">menu3</a></li>\
 	<li><a href="#">menu4</a></li>\
 	<li><a href="#">menu5</a></li>\
   </ul>\
-  <a href="#" onclick="menushow()" class="mobile-menu">MENU</a>\
+  <a onclick="menushow()" class="mobile-menu">MENU</a>\
 </div>';
 
 document.write(header);
