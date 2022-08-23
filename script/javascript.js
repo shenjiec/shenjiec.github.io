abbf240e73e78a271b0609829ea4b53f74ef806e
shenjiec <93009416+shenjiec@users.noreply.github.com>
2022-08-23 16:19:12 +0800

    Update javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -1,4 +1,7 @@
 function menushow(){
 	var menu = document.getElementById('menu');
 	menu.classList.toggle('active');
+	
+	var bd = document.getElementById('bd');
+	bd.classList.toggle('active');
 }
