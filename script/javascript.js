1fc1ae0bf789645a7cd7858d691ca446e69672b0
shenjiec <93009416+shenjiec@users.noreply.github.com>
2022-11-29 13:56:10 +0800

    Update javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -34,10 +34,8 @@ function cal(){
 	array[15] = [1,1,1,1,1,1,1,1,0.5,1,1,1,1,1,1,2,1,0];
 	array[16] = [1,0.5,1,1,1,1,1,2,1,1,1,1,1,2,1,1,0.5,0.5];
 	array[17] = [1,2,1,0.5,1,1,1,1,0.5,0.5,1,1,1,1,1,2,2,1];
-	var a1 = document.getElementById("myPokemon1").value;
-	var a2 = document.getElementById("myPokemon2").value;	
-	var v1 = getValue(a1);
-	var v2 = getValue(a2);
+	var v1 = document.getElementById("myPokemon1").value;
+	var v2 = document.getElementById("myPokemon2").value;
 	var temp0 = "";
 	var temp1 = "";
 	var temp2 = "";
@@ -49,16 +47,16 @@ function cal(){
 		for( i = 0; i < 18; i++){
 			var value = array[i][v1] * array[i][v2];
 			if(value == 0){
-				temp3 = temp3 + getValue2(i) + "傷害無效 傷害: " + value + " 倍" + "\r\n";
+				temp3 = temp3 + getValue(i) + "傷害無效 傷害: " + value + " 倍" + "\r\n";
 			}
 			else if(value > 2){
-				temp0 = temp0 + "弱點 " + getValue2(i) + " 屬性 傷害: " + value + " 倍" + "\r\n";
+				temp0 = temp0 + "弱點 " + getValue(i) + " 屬性 傷害: " + value + " 倍" + "\r\n";
 			}
 			else if(value > 1){
-				temp1 = temp1 + "弱點 " + getValue2(i) + " 屬性 傷害: " + value + " 倍" + "\r\n";
+				temp1 = temp1 + "弱點 " + getValue(i) + " 屬性 傷害: " + value + " 倍" + "\r\n";
 			}
 			else if(value < 1){
-				temp2 = temp2 + "抵抗 " + getValue2(i) + " 屬性 傷害: " + value + " 倍" + "\r\n";
+				temp2 = temp2 + "抵抗 " + getValue(i) + " 屬性 傷害: " + value + " 倍" + "\r\n";
 			}		
 		}		
 		document.getElementById("output0").innerText = temp0;
@@ -71,13 +69,13 @@ function cal(){
 	if(v1 == -1){v1 = v2;}
 	for( i = 0; i < 18; i++){
 		if(array[i][v1] == 0){
-			temp3 = temp3 + getValue2(i) + "傷害無效 傷害: " + array[i][v1] + " 倍" + "\r\n";
+			temp3 = temp3 + getValue(i) + "傷害無效 傷害: " + array[i][v1] + " 倍" + "\r\n";
 		}
 		else if(array[i][v1] > 1){
-			temp1 = temp1 + "弱點 " + getValue2(i) + " 屬性 傷害: " + array[i][v1] + " 倍" + "\r\n";
+			temp1 = temp1 + "弱點 " + getValue(i) + " 屬性 傷害: " + array[i][v1] + " 倍" + "\r\n";
 		}
 		else if(array[i][v1] < 1){
-			temp2 = temp2 + "抵抗 " + getValue2(i) + " 屬性 傷害: " + array[i][v1] + " 倍" + "\r\n";
+			temp2 = temp2 + "抵抗 " + getValue(i) + " 屬性 傷害: " + array[i][v1] + " 倍" + "\r\n";
 		}
 	}
 	document.getElementById("output1").innerText = temp1;
@@ -85,48 +83,6 @@ function cal(){
 	document.getElementById("output3").innerText = temp3;
 }
 function getValue(a){
-	switch(a)
-	{
-		case "一般":		
-			return 0;
-		case "格鬥":		
-			return 1;
-		case "飛行":		
-			return 2;
-		case "毒":		
-			return 3;
-		case "地面":		
-			return 4;
-		case "岩石":		
-			return 5;
-		case "蟲":		
-			return 6;
-		case "幽靈":		
-			return 7;
-		case "鋼":		
-			return 8;
-		case "火":		
-			return 9;
-		case "水":		
-			return 10;
-		case "草":		
-			return 11;
-		case "電":		
-			return 12;
-		case "超能力":		
-			return 13;
-		case "冰":		
-			return 14;
-		case "龍":		
-			return 15;
-		case "惡":		
-			return 16;
-		case "妖精":		
-			return 17;
-	}	
-	return -1;	
-}
-function getValue2(a){
 	switch(a)
 	{
 		case 0:		
