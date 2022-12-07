c9d99dd679eb3ffd6d783a6ef18637ea8b102596
shenjiec <93009416+shenjiec@users.noreply.github.com>
2022-12-07 11:51:10 +0800

    Update javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -5,6 +5,100 @@ function menushow(){
 	var bd = document.getElementById('bd');
 	bd.classList.toggle('active');
 }
+function readPok() {
+	var name = document.getElementById("inputname").value
+	var c1 = myPokemon1.options[myPokemon1.selectedIndex].text;
+	var c2 = myPokemon2.options[myPokemon2.selectedIndex].text;
+	var hp1 = document.getElementById("inputhp1").value;
+	var hp2 = document.getElementById("inputhp2").value;
+	var atk1 = document.getElementById("inputatk1").value;
+	var atk2 = document.getElementById("inputatk2").value;
+	var def1 = document.getElementById("inputdef1").value;
+	var def2 = document.getElementById("inputdef2").value;
+	var spa1 = document.getElementById("inputspa1").value;
+	var spa2 = document.getElementById("inputspa2").value;
+	var spd1 = document.getElementById("inputspd1").value;
+	var spd2 = document.getElementById("inputspd2").value;
+	var spe1 = document.getElementById("inputspe1").value;
+	var spe2 = document.getElementById("inputspe2").value;
+	var tot1 = document.getElementById("inputtot1").value;
+	var tot2 = document.getElementById("inputtot2").value;
+	
+	var dataUrl= "https://shenjiec.github.io/json/data.json"
+	$.ajax({
+		url: dataUrl,
+		method: 'GET',
+		dataType: 'json',
+		data: '',
+		async: true,
+	   
+		success: res =>{
+				//console.log(res)
+				var count = 0;
+				var temp = "<tr><td style=\"width:30px\">編號</td><td style=\"width:80px\">名稱</td><td colspan=2 style=\"width:120px\">屬性</td><td style=\"width:21px\">HP</td><td style=\"width:21px\">ATK</td><td style=\"width:21px\">DEF</td><td style=\"width:21px\">SPA</td><td style=\"width:21px\">SPD</td><td style=\"width:21px\">SPE</td><td style=\"width:21px\">TOT</td></tr>";
+				for (i = 0; i < res.length; i++) {
+					if(name != "" && !res[i].name.includes(name)){
+						continue
+					}
+					if(c1 != "" && res[i].c1 != c1 && res[i].c2 != c1){
+						continue;
+					}
+					if(c2 != "" && res[i].c1 != c2 && res[i].c2 != c2){
+						continue;
+					}
+					if(hp1 != "" && res[i].hp < hp1){
+						continue;
+					}
+					if(hp2 != "" && res[i].hp > hp2){
+						continue;
+					}
+					if(atk1 != "" && res[i].atk < atk1){
+						continue;
+					}
+					if(atk2 != "" && res[i].atk > atk2){
+						continue;
+					}
+					if(def1 != "" && res[i].def < def1){
+						continue;
+					}
+					if(def2 != "" && res[i].def > def2){
+						continue;
+					}
+					if(spa1 != "" && res[i].spa < spa1){
+						continue;
+					}
+					if(spa2 != "" && res[i].spa > spa2){
+						continue;
+					}
+					if(spd1 != "" && res[i].spd < spd1){
+						continue;
+					}
+					if(spd2 != "" && res[i].spd > spd2){
+						continue;
+					}
+					if(spe1 != "" && res[i].spe < spe1){
+						continue;
+					}
+					if(spe2 != "" && res[i].spe > spe2){
+						continue;
+					}
+					if(tot1 != "" && res[i].tot < tot1){
+						continue;
+					}
+					if(tot2 != "" && res[i].tot > tot2){
+						continue;
+					}
+					count++;
+					temp += "<tr><td>" + res[i].no + "</td><td>" + res[i].name + "</td>" + getAttribute(res[i].c1, res[i].c2) + "<td>" + res[i].hp + "</td><td>" + res[i].atk + "</td><td>" + res[i].def + "</td><td>" + res[i].spa + "</td><td>" + res[i].spd + "</td><td>" + res[i].spe + "</td><td>" + res[i].tot + "</td></tr>";
+				}
+				document.getElementById("mytable").innerHTML = temp;
+				document.getElementById("output1").innerText = count + " 符合結果";
+			},
+		error: err =>{
+				console.log(err)
+			},
+	});
+}
 function clr(){
 	document.getElementById("myPokemon1").selectedIndex = 0;
 	document.getElementById("myPokemon2").selectedIndex = 0;
@@ -12,6 +106,27 @@ function clr(){
 	document.getElementById("output1").innerText = "";
 	document.getElementById("output2").innerText = "";
 	document.getElementById("output3").innerText = "";
+}
+function clr2(){
+	document.getElementById("myPokemon1").selectedIndex = 0;
+	document.getElementById("myPokemon2").selectedIndex = 0;
+	document.getElementById("inputname").value = "";
+	document.getElementById("inputhp1").value = "";
+	document.getElementById("inputhp2").value = "";
+	document.getElementById("inputatk1").value = "";
+	document.getElementById("inputatk2").value = "";
+	document.getElementById("inputdef1").value = "";
+	document.getElementById("inputdef2").value = "";
+	document.getElementById("inputspa1").value = "";
+	document.getElementById("inputspa2").value = "";
+	document.getElementById("inputspd1").value = "";
+	document.getElementById("inputspd2").value = "";
+	document.getElementById("inputspe1").value = "";
+	document.getElementById("inputspe2").value = "";
+	document.getElementById("inputtot1").value = "";
+	document.getElementById("inputtot2").value = "";
+	document.getElementById("mytable").innerHTML = "";
+	document.getElementById("output1").innerText = "";
 }
 function cal(){
 	document.getElementById("output0").innerText = "";
@@ -122,5 +237,55 @@ function getValue(a){
 		case 17:		
 			return "妖精";
 	}	
+	return "";
+}
+function getAttribute(a, b){
+	if(b == ""){		
+		return "<td colspan=2 style=\"" + getColor(a) + "\">" + a + "</td>";
+	}
+	else{
+		return "<td style=\"" + getColor(a) + "\">" + a + "</td><td style=\"" + getColor(b) + "\">" + b + "</td>";		
+	}
+}
+function getColor(a){
+	switch(a)
+	{
+		case "一般":
+			return "background-Color:lightgray; color:black;";
+		case "格鬥":
+			return "background-Color:coral; color:white;";
+		case "飛行":
+			return "background-Color:lightblue; color:black;";
+		case "毒":
+			return "background-Color:orchid; color:white;";
+		case "地面":
+			return "background-Color:chocolate; color:white;";
+		case "岩石":
+			return "background-Color:tan; color:white;";
+		case "蟲":
+			return "background-Color:darkkhaki; color:white;";
+		case "幽靈":
+			return "background-Color:mediumpurple; color:white;";
+		case "鋼":
+			return "background-Color:steelblue; color:white;";
+		case "火":
+			return "background-Color:red; color:white;";
+		case "水":
+			return "background-Color:dodgerblue; color:white;";
+		case "草":
+			return "background-Color:mediumseagreen; color:white;";
+		case "電":
+			return "background-Color:gold; color:white;";
+		case "超能力":
+			return "background-Color:lightcoral; color:white;";
+		case "冰":
+			return "background-Color:cyan; color:black;";
+		case "龍":
+			return "background-Color:royalblue; color:white;";
+		case "惡":
+			return "background-Color:saddlebrown; color:white;";
+		case "妖精":
+			return "background-Color:hotpink; color:white;";
+	}
 	return "";	
 }
