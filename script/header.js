1f06b7ef80e4d625cb3465753314dee79904e21f
shenjiec <93009416+shenjiec@users.noreply.github.com>
2022-12-23 16:17:15 +0800

    Update header.js

--- script/header.js
+++ script/header.js
@@ -6,7 +6,7 @@ header ='<div id="bd" class="bd"></div>\
   <ul id="menu" class="menu">\
 	<li><a href="index.html">Home</a></li>\
 	<li><a href="pokeMenu.html">Pokemon</a></li>\
-	<li><a href="#">menu3</a></li>\
+	<li><a href="soloMenu.html">SoloKnight</a></li>\
 	<li><a href="#">menu4</a></li>\
 	<li><a href="#">menu5</a></li>\
   </ul>\
